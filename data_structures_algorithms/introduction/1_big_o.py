010b5972a58c3cba8bbf6607e3bbf70f1b5fd4d4
riley <rileyrockwell@gmail.com>
2023-09-01 19:45:16 -0700

    data structures algos intro

---
+++ data_structures_algorithms/introduction/1_big_o.py
@@ -0,0 +1,31 @@
+# time complexity: amount of time the algorithm needs to run relative to the input size.
+# logarithmic
+def function(parameter):
+	pass
+
+# linear
+def function1(parameter):
+	pass
+
+# log linear
+def function2(parameter):
+	pass
+
+# quadratic
+def function3(parameter):
+	pass
+
+# cubic
+def function4(parameter):
+	pass
+
+# exponential
+def function5(parameter):
+	pass
+
+# space complexity: amount of memory allocated by the algorithm when run relative to the input size.
+
+"""
+-how does the algorithm scale with the input size?
+-
+"""
