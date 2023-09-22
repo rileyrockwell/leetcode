b2abcf5afa9200e57428a330eca4d5a00fc952c4
riley <rileyrockwell@gmail.com>
2023-09-22 10:36:28 -0700

    sliding window

--- data_structures_algorithms/2_arrays_strings/2.20_sliding_window.py
+++ data_structures_algorithms/2_arrays_strings/2.20_sliding_window.py
@@ -1,7 +1,7 @@
-# example functions
-def find_length(nums, k):
-	# use sliding window technique
-	left = curr = ans = 0
+# 1. find_length(nums, k)
+
+def find_length_nums(nums, k):
+	left = curr = answer = 0
 
 	for right in range(len(nums)):
 		curr += nums[right]
@@ -10,156 +10,80 @@ def find_length(nums, k):
 			curr -= nums[left]
 			left += 1
 
-		ans = max(ans, right - left + 1)
+		answer = max(answer, right - left + 1)
 
-	return ans
+	return answer
 
 
-def find_length1(s):
-	# how to set up the sliding window?
-	# do the video explanations do anything for you?
-	# least effecitve (greatest time suck): reading
-	# most effective: learning w/ other people
-	# 
-	pass
+# 2. find_length(s)
 
+def find_length(s):
+	left = curr = answer = 0
+	
+	for right in range(len(s)):
+		if s[right] == "0":
+			curr += 1
+
+		while curr > 1:
+			if s[left] == "0":
+				curr -= 1
+			left += 1
+
+		answer = max(answer, right - left + 1)
+
+	return answer
+
+
+# 3. numSubarrayProductLessThanK(nums, k)
 
 def numSubarrayProductLessThanK(nums, k):
-	return 1
+	if k <= 1:
+		return 1
+
+	answer = left = 0
+	curr = 1
+
+	for right in range(len(nums)):
+		curr *= nums[right]
+		while curr >= k:
+			curr //= nums[left]
+			left += 1
+		answer += right - left + 1
+
+	return answer
+
+
+# 4. find_best_subarray(nums, k):
 
 def find_best_subarray(nums, k):
-	return 1
-
-#		time complexity			space complexity
-# (1).	...						...
-# (2).	...						...
-# (3).  ...						...
-# (4).  ...						...
+	curr = 0
+	
 
 
-
-### 9.20: 02:45 ###
-
-# ex 1
-def find_length(nums, k):
-	left = curr_sum = ans = 0
-	for right in range(len(nums)):
-		curr_sum += nums[right]
-		while curr_sum > k:
-			curr_sum -= nums[left]
-			left += 1
-		ans = max(ans, right - left + 1)
-
-	return ans
+###
+print(find_length_nums([0, 1, 2, 3], 4))
+print(find_length("11001011"))
+print(numSubarrayProductLessThanK([2, 4], 8))
 
 
 
-"""
-do not move on until mastered. one step at a time. but broski: have fun along the way. it's okay to have fun while you are working.
-be of service and make an effort to cotnribute more than you consume. it brings far more mental wellbeing.
-"""
 
 
-### 9.20: 09:00 ###
-def find_length(nums, k):
-	left = curr_sum = answer = 0
+### 
+# (1). O(n) | O(1)
+# objective: find the length of the longest substring (window) with sum is <= k.
 
-	# k: length of the sliding window?
-	for right in range(len(nums)):
-		curr_sum += nums[right]
-		
-		# ideally we stay at curr_sum <= k; if not reduce the size of the sliding window from the left
-		while curr_sum > k:
-			curr_sum -= nums[left]
-			left += 1
+# (2). O(n) | O(1)
+# objective: find the longest subarray of at most one "0" element. (b/c we can flip at most one "0" to a "1").
 
-			answer = max(answer, right - left + 1)
+# (3). O(n) | O(1)
+# objective: find the number of subarrays s.t. the product of the elements in the subarray is < k.
 
-	return answer
+# (4). O(n) | O(1)
+# objective: ...
 
-
-def find_length(nums, k):
-	# return: length of the longest subarray whose sum is less than or equal to k
-
-	left = curr_sum = answer = 0
-
-	for right in range(len(nums)):
-		curr_sum += nums[right]
-
-		# ideally curr_sum is <= k, in which case nums does not have any negative integers?
-		while curr_sum > k:
-			curr_sum -= nums[left]
-			left += 1
-
-		answer = max(answer, right - left + 1)
-
-	return answer
-
-
-def find_lengths(nums, k):
-	left = curr_sum = answer = 0
-
-	for right in range(len(nums)):
-		curr_sum += nums[right]
-
-		# if current sliding window sums to an integer larger than k, reduce the size of the sliding window from the left?
-		while curr_sum > k:
-			curr_sum -= nums[left]
-			left -= 1
-
-		answer = max(answer, right - left + 1)
-
-	return answer
-
-def find_length(nums, k):
-	left = curr_sum = answer = 0
-
-	for right in range(len(nums)):
-		curr_sum += nums[right]
-
-		while curr_sum > k:
-			# reduce the size of the sliding window from the left, so that the sum stays under the upper bound, k
-			curr_sum -= nums[left]
-			left += 1
-
-		# length > sum | of nums
-		print(answer, right - left + 1)
-
-		answer = max(answer, right - left + 1)
-
-	return answer
-
-
-nums = list(range(10))
-k = 7
-print(find_length(nums, k))
-
-
-### 9.20 | 11:37 ###
-def find_length(s):
-	left = curr_sum = answer = 0
-	for right in range(len(s)):
-		pass
-
-### 9.20 | 16:24 ###
-def find_length(s):
-	pass
-
-### 9.20 | 17:06 ###
-def find_best_subarray(nums, k):
-	curr_sum = 0
-	for i in range(k):
-		curr_sum += nums[i]
-
-	answer = curr_sum
-	for i in range(k, len(nums)):
-		curr_sum += nums[i] - nums[i - k]
-		answer = max(answer, curr_sum)
-
-	return answer
-
-print("###")
-
-nums = list(range(6))
-k = 3
-print(find_best_subarray(nums, k))
+### 	10:30		12:00
+# (1). 	0.35		...
+# (2). 	0.20		...
+# (3). 	0.20		...
+# (4). 	0.10		...
--- data_structures_algorithms/2_arrays_strings/3.py
+++ data_structures_algorithms/2_arrays_strings/3.py
@@ -66,4 +66,93 @@ def numSubarrayProductLessThanK(nums, int):
 
 
 def numSubarrayProductLessThanK(nums, k):
-	pass
+	# base case, 1 b/c of multiplcation
+	if k <= 1:
+		return 1
+
+	left = ans = 0
+	curr = 0
+
+	# implement sliding window technique
+	for right in range(len(nums)):
+		# increase current sum by each iteration of the right bound of the sliding window
+		curr = curr * nums[right]
+
+		# to ensure sliding window fits the parameter constraints
+		while curr >= k:
+			# explain...do not move on until understood.
+			curr = curr // nums[left]
+
+			# move to a new starting elment for the window
+			left += 1
+
+		answer = max(answer, right - left + 1)
+
+
+
+def numSubarrayProductLessThanK(nums, k):
+	left = answer = 0
+	curr = 1 # b/c of multiplication
+
+	for right in range(len(nums)):
+		# ideally k < curr
+		curr = curr * nums[right]
+
+		# not ideal condition
+		while curr >= k:
+			# do the opposite of curr = curr * nums[right]
+			curr = curr // nums[right]
+
+			# b/c you are not iterating over left
+			left += 1
+
+	# right - left + 1: right (ending index of the window), left (starting index of the window), + 1 (b/c range(len(nums)))
+
+	# right - left: e.g. right = 10, left = 3; right - left = 7; + 1 b/c ...
+		answer = max(answer, right - left + 1)
+
+	return answer
+
+
+nums = list(range(6))
+k = 3
+
+print(nums)
+print(numSubarrayProductLessThanK(nums, k))
+
+
+
+def numSubarrayProductLessThanK(nums, k):
+	if k <= 1:
+		return 0
+
+	left = answer = 0
+	curr = 1
+
+	for right in range(len(nums)):
+		curr *= curr[right]
+
+		# when the window is broken
+		while curr >= k:
+			# perform the inverse
+			# curr = curr // nums[right]
+			curr //= nums[right]
+
+			# redefine the left bound of the sliding window, b/c constraint was broken (while loop)
+			left += 1
+
+		# "once we know that the windows is valid, we know that the number of subarrays that end at the current index is right - left + 1"
+		answer += right - left + 1
+
+	return answer
+
+
+
+
+
+
+
+"""
+if you fix the right bound, the left bound can take any value of the current window.
+therefore, the size of the window is the number of valid subarrrays.
+
