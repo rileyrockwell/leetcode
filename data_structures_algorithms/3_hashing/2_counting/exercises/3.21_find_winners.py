93c11b67c4ee90d08cb6ef077cad740bc3d2b24e
riley <rileyrockwell@gmail.com>
2023-10-13 14:56:28 -0700

    updates

--- data_structures_algorithms/3_hashing/2_counting/exercises/3.21_find_winners.py
+++ data_structures_algorithms/3_hashing/2_counting/exercises/3.21_find_winners.py
@@ -1,6 +1,6 @@
 class Solution:
     def find_winners(self, matches: [[int]]) -> [[int]]:
-        return 0
+        return 
 
 matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
 print(Solution().find_winners(matches))
