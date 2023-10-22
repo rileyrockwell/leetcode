6d01401a95cb7b50e13cc98d6372967446cd0f63
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2023-10-21 20:38:27 -0700

    updates

---
+++ data_structures_algorithms/3_hashing/321_find_winners.py
@@ -0,0 +1,20 @@
+class Solution: 
+    def find_winners(self, matches: [[int]]) -> [[int]]: 
+        losses_count = {}
+        
+        for winner, loser in matches:
+            losses_count[winner] = losses_count.get(winner, 0)
+            losses_count[loser] = losses_count.get(loser, 0) + 1
+        
+        zero_lose, one_lose = [], []
+        for player, count in losses_count.items():
+            if count == 0:
+                zero_lose.append(player)
+            if count == 1:
+                one_lose.append(player)
+        
+        return [sorted(zero_lose), sorted(one_lose)]
+
+
+matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+print(Solution().find_winners(matches))
