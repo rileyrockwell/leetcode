c1424e9b76c54b11048107f4f7be3375088dcd53
riley <rileyrockwell@gmail.com>
2023-10-24 00:46:12 -0700

    update

--- data_structures_algorithms/3_hashing/3_more_hashing_examples/exercises/3.31_can_construct.py
+++ data_structures_algorithms/3_hashing/3_more_hashing_examples/exercises/3.31_can_construct.py
@@ -10,9 +10,34 @@ class Solution:
 		for letter in magazine:
 			magazine_dic[letter] = len(list(i for i in magazine if i == letter))
 
-		return ransom_note_dic, magazine_dic
+		subset = ransom_note_dic
+		orig_set = magazine_dic
 
+		for key, value in subset.items():
+			if key not in orig_set.keys():
+				# key not in parent set
+				return False
+			elif subset[key] > orig_set[key]:
+				# not enough letters in parent set to satisfy subset
+				return False
+
+		
+		# if for every key in subset there is a key in orig_set with a value greater than or equal
+		# to the value in subset
+		return True
+
+ransom_note = "a"
+magazine = "b"
+print(Solution().can_construct(ransom_note, magazine))
+
+ransom_note = "aa"
+magazine = "ab"
+print(Solution().can_construct(ransom_note, magazine))
 
 ransom_note = "aa"
 magazine = "aab"
+print(Solution().can_construct(ransom_note, magazine))
+
+ransom_note = "fihjjjjei"
+magazine = "hjibagacbhadfaefdjaeaebgi"
 print(Solution().can_construct(ransom_note, magazine))
---
+++ data_structures_algorithms/3_hashing/3_more_hashing_examples/exercises/3.32_num_jewels_in_stones.py
@@ -0,0 +1,7 @@
+class Solution:
+	def numJewelsInStones(self, jewels: str, stones: str) -> int:
+		
+
+jewels = "aA"
+stones = "aAAbbbb"
+print(Solution().numJewelsInStones(jewels, stones))
