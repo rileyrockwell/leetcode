7b31672f871fa67e07adb4fd7895b75c667e74ab
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2023-10-31 12:10:03 -0700

    updates

--- data_structures_algorithms/3_hashing/bonus/2.4.py
+++ data_structures_algorithms/3_hashing/bonus/2.4.py
@@ -4,8 +4,19 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         dict_freq = Counter(s)
 
-        
+        return dict_freq
 
-print(Solution().frequencySort("tree"))
-print(Solution().frequencySort("cccaaa"))
-print(Solution().frequencySort("Aabb"))
+        return_string = ""
+        for key, value in temp_list:
+            return_string += key * value
+    
+        return return_string
+
+
+
+print(Solution().frequencySort("zraaeayederex"))
+
+
+# print(Solution().frequencySort("tree"))
+# print(Solution().frequencySort("cccaaa"))
+# print(Solution().frequencySort("Aabb"))
---
+++ data_structures_algorithms/3_hashing/bonus/2.5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numIdenticalPairs(self, nums: [int]) -> int:
+        pass
+
+
+
+
+t1 = [1, 2, 3, 1, 1, 3]
+t2 = [1, 1, 1, 1]
+t3 = [1, 2, 3]
+
+for nums in [t1, t2, t3]:
+    print(Solution().numIdenticalPairs(nums))
