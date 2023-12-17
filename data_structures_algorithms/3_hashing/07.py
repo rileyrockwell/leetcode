4860aedc03a57f6806a1f544e6649e8dad27d12d
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2023-12-17 00:16:58 -0800

    updates

--- data_structures_algorithms/3_hashing/07.py
+++ data_structures_algorithms/3_hashing/07.py
@@ -1,19 +1,42 @@
-from typing import *
+# semi-copied from GPT
+# hashing: counting
+# copied from GPT
 
 class Solution:
-    def findMaxLength(self, arr: List[int]) -> int:
+    def countContiguousSubarrays(self, arr):
         n = len(arr)
-        answer = n
-        
-        for start in range(n):
-            for end in range(start, n):
-                # build each individual subarray (including the trivial subarray)
-                subarray = arr[start:end + 1]
-                # for visualization: print(subarray)
-                answer += 1
+        counter = 0
 
-        return answer
+        # sliding window...
+        valid_subarray = 0
+
+        left = 0
 
 
-nums = list(range(6))
-print(Solution().findMaxLength(nums))
+        for right in range(1, n + 1):
+            window = arr[:right]
+
+            # determine if integers of window are in consecutive increasing order
+            for index in range(len(window)):
+                if arr[i] <= arr[i - 1]:
+                # if s_n <= s_n-1, forget about that window; break and redefine the window
+                    break
+                else:
+                    valid_subarray += 1
+
+            # under what conditions will left increasing by 1?
+            left += 1
+
+
+# Example usage
+
+# always start w/ the most basic "case"
+arr = [0, 0]
+
+# arr = [0, 1]
+# arr = [0, 1, 0]
+# arr = [6, 3 ,4, 8, 10, 5, 7, 1, 9, 2]
+arr = [1, 2, -3, 4, -1, 2, 1, -5, 4]
+result = Solution().countContiguousSubarrays(arr)
+
+print("Number of contiguous subarrays:", result)
