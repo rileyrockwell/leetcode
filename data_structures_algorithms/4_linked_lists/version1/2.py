0ef4850649eedb983ee2b554904e634d41584e4d
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2023-12-22 11:24:48 -0800

    updates

--- data_structures_algorithms/4_linked_lists/version1/2.py
+++ data_structures_algorithms/4_linked_lists/version1/2.py
@@ -7,7 +7,14 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head):
-        
+        # riley, if you are blessed enough to be behind a computer, you must exert yourself
+        # in the same capacity as the laborer.
+
+        # riley: self-compassion. think about it, in your down time.
+        return 1
 
 
 head = ListNode(0)
+
+instance = Solution().deleteDuplicates(head)
+print(instance)
