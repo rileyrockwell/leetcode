5d89479b3d72864b79d2e516736c6772b0661ccc
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2024-03-26 23:49:15 -0700

    updates

---
+++ 1_arrays/26_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,12 @@
+from typing import *
+
+def removeDuplicates(nums: List[int]) -> int:
+	pass
+
+
+
+nums = [1, 1, 2]
+print(removeDuplicates(nums))
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+print(removeDuplicates(nums))
